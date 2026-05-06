f3f06418e9aeaab6b6106107235d758d2af032c1
Douglas Henrique Pereira Leite <1051570@senacminas.edu.br>
2026-05-06 11:51:33 -0300

    formulario

---
+++ estações/script.js
@@ -0,0 +1,12 @@
+function verao(){
+    document.getElementById("arvore_img").src='verao.jpg';
+}
+function outono(){
+    document.getElementById("arvore_img").src='outono.jpg';
+}
+function primavera(){
+    document.getElementById("arvore_img").src='primavera.jpg';
+}
+function inverno(){
+    document.getElementById("arvore_img").src='inverno.jpg';
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoPremio = document.querySelector('.dados');
+
+botaoPremio.addEventListener('click', () => {
+  alert('Formulário Enviado!');
+}, { once: true });
